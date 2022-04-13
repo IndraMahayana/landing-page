756ec7f3f28ac12180e77d154c26fc417267b889
Jas <hacyonukumori@gmail.com>
2022-04-14 02:06:50 +0800

    landing-page

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const navbar = document.querySelector('.navbar')
+const changeColor1 = document.querySelector('.a-color1')
+const changeColor2 = document.querySelector('.a-color2')
+const changeColor3 = document.querySelector('.a-color3')
+const changeColor4 = document.querySelector('.a-color4')
 
 window.onscroll = () => {
     if (window.scrollY > 20) {
         navbar.classList.add('scroll-navbar')
+        changeColor1.classList.add('change-a-color')
+        changeColor2.classList.add('change-a-color')
+        changeColor3.classList.add('change-a-color')
+        changeColor4.classList.add('change-a-color')
     } else {
         navbar.classList.remove('scroll-navbar')
+        changeColor1.classList.remove('change-a-color')
+        changeColor2.classList.remove('change-a-color')
+        changeColor3.classList.remove('change-a-color')
+        changeColor4.classList.remove('change-a-color')
     }
 }
